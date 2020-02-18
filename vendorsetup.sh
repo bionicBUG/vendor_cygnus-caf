Wave-Project/vendor_wave

vendor: enable Snapdragon LLVM 8.0 Compiler
passes CTS and has no stability issues.

also reverts ''''
sdclang: Add Pie configuration (a3fb97e3af54dd704ad1c560600860390e22eb53)
''''

Change-Id: I6d46ef2c208fecb8c8032c5ceb3bec09bc6e74b4

vendorsetup.sh
@@ -3,3 +3,8 @@ caf_devices=('rolex' 'oneplus3' 'A6020')
 for device in ${caf_devices[@]}; do
     add_lunch_combo wave_$device-userdebug
 done
+
+# SDClang Environment Variables
+export SDCLANG_AE_CONFIG=$(pwd)/vendor/wave/sdclang/sdclangAE.json
+export SDCLANG_CONFIG=$(pwd)/vendor/wave/sdclang/sdclang.json
+export SDCLANG_SA_ENABLED=false

bionicBUG/vendor_cygnus-caf

vendorsetup.sh
@@ -1,4 +1,4 @@
 # SDClang Environment Variables
-export SDCLANG_AE_CONFIG=vendor/cygnus/sdclang/sdclangAE.json
-export SDCLANG_CONFIG=vendor/cygnus/sdclang/sdclang.json
+export SDCLANG_AE_CONFIG=$(pwd)/vendor/cygnus/sdclang/sdclangAE.json
+export SDCLANG_CONFIG=$(pwd)/vendor/cygnus/sdclang/sdclang.json
 export SDCLANG_SA_ENABLED=false